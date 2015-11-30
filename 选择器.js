20adaa4ae5f8ccafa804a9cf8302f0f6aac407a9
josslynmao <mao.huifeng@hotmail.com>
2015-11-30 20:38:08 +0800

    selector

---
+++ 选择器.js
@@ -0,0 +1,5 @@
+$("#one").css("background","#bbffaa");
+$(".mini").css("background","#AFEBFF");
+$("div").css("background","#FFF7BD");
+$("*").css("background","#bbffaa");
+$("span,#two").css("background","#AFEBFF");
