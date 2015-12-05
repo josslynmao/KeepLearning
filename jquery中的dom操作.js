10f34a0de70c1fd492b429f9593826f5c4fbd059
josslynmao <mao.huifeng@hotmail.com>
2015-12-05 14:40:02 +0800

    xxx

---
+++ jquery中的dom操作.js
@@ -0,0 +1,32 @@
+/*var $li = $("ul li:eq(1)");
+var li_txt = $li.text();
+alert(li_txt);
+
+var $p = $("p");
+var title = $p.attr("title");
+alert(title);
+
+var $li_1 = $("<li title='香蕉'>香蕉</li>");
+var $li_2 = $("<li title'橙子'>橙子</li>");
+$("ul").append($li_1);
+$("ul").append($li_2);
+
+var $li_a = $("ul li:eq(0)");
+var $li_b = $("ul li:eq(1)");
+$li_a.before($li_b);*/
+
+//remove()
+/*var $li = $("ul li:eq(1)").remove();
+$li.appendTo("ul");*/
+
+//detach()
+/*$("ul li").click(function(){
+	alert("click it!");
+});
+
+var $li = $("ul li:eq(1)").detach();
+$li.appendTo("ul");*/
+
+//empty
+var $li = $("ul li:eq(0)").empty();
+$li.appendTo("ul");
